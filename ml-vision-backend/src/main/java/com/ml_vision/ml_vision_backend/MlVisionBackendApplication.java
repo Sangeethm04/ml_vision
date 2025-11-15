68b938aa34fe8d49174fc7691eddc64fb2baba3e
dpm227 <dpm227@lehigh.edu>
2025-11-15 13:28:17 -0500

    added ml-vision-backend

---
+++ ml-vision-backend/src/main/java/com/ml_vision/ml_vision_backend/MlVisionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ml_vision.ml_vision_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlVisionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlVisionBackendApplication.class, args);
+	}
+
+}
---
+++ ml-vision-backend/src/test/java/com/ml_vision/ml_vision_backend/MlVisionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml_vision.ml_vision_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlVisionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
